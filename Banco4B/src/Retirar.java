83b265b18bf84ece270009ceb3a537a95852b451
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-09-05 13:12:51 -0600

    Tambien agregue los getters para hacerlo mas inovador y funcional

--- Banco4B/src/Retirar.java
+++ Banco4B/src/Retirar.java
@@ -1,19 +1,16 @@
 public class Retirar {
-    public static double realizarRetiro(double saldoActual, double cantidad) {
+    public static void realizarRetiro(Cuenta cuenta, double cantidad) {
         if (cantidad <= 0) {
-            System.out.println("Error: La cantidad debe ser mayor a cero");
-            return saldoActual;
+            System.out.println("Esta mal la cantidad debe ser mayor a cero");
+            return;
         }
 
-        if (cantidad > saldoActual) {
-            System.out.println("Error: Fondos insuficientes");
-            System.out.println("Saldo disponible: $" + saldoActual);
-            return saldoActual;
+        if (cuenta.retirar(cantidad)) {
+            System.out.println("Retiro exitoso: $" + cantidad);
+            System.out.println("Nuevo saldo: $" + cuenta.getSaldo());
+        } else {
+            System.out.println("Error no tienes feria");
+            System.out.println("Saldo disponible: $" + cuenta.getSaldo());
         }
-
-        double nuevoSaldo = saldoActual - cantidad;
-        System.out.println("Retiro exitoso: $" + cantidad);
-        System.out.println("Nuevo saldo: $" + nuevoSaldo);
-        return nuevoSaldo;
     }
 }
